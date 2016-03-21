a3977f7de16222b1a73d71d43440c3bad3c364a1
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-03-21 10:48:09 +0100

    Migrations & Models - Default

---
+++ database/migrations/2016_03_21_094725_create_blacklists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlacklistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blacklists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blacklists');
+    }
+}
