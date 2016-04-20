ea659b7d6f4cf5a1d7ff7bec9a73c4e5c1c43ab1
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-04-20 10:26:51 +0200

    SessionController - Basic structure of functions and documentation

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -8,5 +8,77 @@ use App\Http\Requests;
 
 class SessionController extends Controller
 {
-    //
+	/**
+	 * Show an overview of the walls.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new wall.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created wall in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified wall.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified wall.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified wall in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove (inactive) the specified wall from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
