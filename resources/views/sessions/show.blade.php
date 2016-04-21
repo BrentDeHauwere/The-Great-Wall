b42da3c65ddf553aabc00abfae9f0e404d3a07e5
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-04-21 11:50:39 +0200

    SessionController - Action show: done

--- resources/views/sessions/show.blade.php
+++ resources/views/sessions/show.blade.php
@@ -1,7 +1,13 @@
 @extends('master')
 
-@section('title', 'Wall')
+@section('title', 'Wall ' . $wall->name)
 
 @section('content')
-	<!-- Content -->
+	<div class="jumbotron text-center">
+		<h2>{{ $wall->name }}</h2>
+		<p>
+			<strong>UserID:</strong> {{ $wall->user_id }}<br>
+			<strong>Password:</strong> {{ $wall->password }}
+		</p>
+	</div>
 @endsection
