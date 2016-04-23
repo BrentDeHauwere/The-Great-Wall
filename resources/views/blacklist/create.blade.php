b04782c122a2bcc6bc0a565f655952d464cdff08
jonas.de.pelsmaeker <jonas.de.pelsmaeker@student.ehb.be>
2016-04-23 20:58:37 +0200

    User kan nu geband worden via message, nog niet via poll

---
+++ resources/views/blacklist/create.blade.php
@@ -0,0 +1,27 @@
+@extends('master')
+
+@section('title', 'Ban User')
+
+@section('content')
+	<h1>Ban user - {{ $user_id }}</h1>
+
+	 @if (count($errors) > 0)
+		<div class="alert alert-danger">
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
+
+	<form action="{{ action('BlacklistController@store') }}" method="POST">
+		<div class="form-group">
+			<label for="reason">Reason</label>
+			<input id="reason" type="text" name="reason" class="form-control" autofocus="">
+		</div>
+    <input type="hidden" name="user_id" value="{{ $user_id }}">
+		{{ csrf_field() }}
+		<input type="submit" value="Ban" class="btn btn-primary">
+	</form>
+@endsection
