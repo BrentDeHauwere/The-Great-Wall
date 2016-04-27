1b8d4fa0b99f624cdcb5a26553d7d5ee4973e59c
jonas.de.pelsmaeker <jonas.de.pelsmaeker@student.ehb.be>
2016-04-27 20:43:56 +0200

    API: messages worden gereturned zoals het afgesproken formaat in JSON-response

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,27 +16,10 @@ class ApiController extends Controller
 {
   public function walls(){
 
-    $walls = DB::table('walls')->select('id', 'name', 'user_id', 'created_at')->get();
+    $walls = Wall::all();
 
-    foreach($walls as $wall){
-
-      //id formatting to wall_id
-      $wall->wall_id = $wall->id;
-      unset($wall->id);
-
-      //user_id formatting to creator{userid, name}
-      $user = User::find($wall->user_id);
-
-      if (empty($user->name)){
-        $wall->creator = ["user_id" => $wall->user_id, "name" => "Not found."];
-      } else {
-        $wall->creator = ["user_id" => $wall->user_id, "name" => $user->name];
-      }
-
-      unset($wall->user_id);
-
-      //Convert timestamp to unix format
-      $wall->created_at = strtotime($wall->created_at);
+    for($i = 0; $i < count($walls); $i++){
+      $walls[$i] = ApiController::formatWall($walls[$i]);
     }
 
     return response()->json($walls);
@@ -47,33 +30,7 @@ class ApiController extends Controller
     $messages = Message::all();
 
     foreach($messages as $msg){
-      //format id to message_id
-      $msg->message_id = $msg->id;
-      unset($msg->id);
-
-      //format user_id to creator{userid, name} if not anonymous
-      if ($msg->anonymous == 1){
-        $msg->creator = null;
-      } else {
-        $user = User::find($msg->user_id);
-        $msg->creator = ["user_id" => $msg->user_id, "name" => $user->name];
-      }
-      unset($msg->anonymous);
-
-      unset($msg->moderation_level);
-
-      //format count to votes
-      $msg->votes = $msg->count;
-      unset($msg->count);
-
-      //if message is a response to another message, send message with repsonse message
-      if(!empty($msg->question_id)){
-        
-      }
-
-      //format timestamp to unix format
-      $msg->created_at = strtotime($msg->created_at);
-
+      ApiController::formatMessage($msg);
     }
 
     return response()->json($messages);
@@ -100,5 +57,72 @@ class ApiController extends Controller
     }
 
     return response()->json($db);
+  }
+
+  private function formatMessage(Message $msg){
+    //format id to message_id
+    $msg->message_id = $msg->id;
+    unset($msg->id);
+
+    //format user_id to creator{userid, name} if not anonymous
+    if ($msg->anonymous == 1){
+      $msg->creator = null;
+    } else {
+      $user = User::find($msg->user_id);
+      $msg->creator = ["user_id" => $msg->user_id, "name" => $user->name];
+    }
+    unset($msg->user_id);
+    unset($msg->anonymous);
+    unset($msg->moderation_level);
+    unset($msg->channel_id);
+
+    //format count to votes
+    $msg->votes = $msg->count;
+    unset($msg->count);
+
+    //format question_id to question_id
+    $msg->question = $msg->question_id;
+    unset($msg->question_id);
+
+    //if message is a response to another message, send message with repsonse message
+    if(!empty($msg->question)){
+      $msg2 = Message::find($msg->question);
+      $msg->question = ApiController::formatMessage($msg2);
+    }
+
+    //format wall_id to wall{wall_id, name, creator{user_id, name}}
+    $wall = Wall::findOrFail($msg->wall_id);
+    unset($msg->wall_id);
+    $msg->wall = ApiController::formatWall($wall);
+
+    //format timestamp to unix format
+    $msg->created_at = strtotime($msg->created_at);
+
+    return $msg;
+  }
+
+  private function formatWall(Wall $wall){
+    //id formatting to wall_id
+    $wall->wall_id = $wall->id;
+    unset($wall->id);
+
+    //user_id formatting to creator{userid, name}
+    $user = User::find($wall->user_id);
+
+    if (empty($user->name)){
+      $wall->creator = ["user_id" => $wall->user_id, "name" => "Not found."];
+    } else {
+      $wall->creator = ["user_id" => $wall->user_id, "name" => $user->name];
+    }
+
+    unset($wall->user_id);
+    unset($wall->password);
+    unset($wall->open_until);
+    unset($wall->deleted_at);
+
+    //Convert timestamp to unix format
+    $wall->created_at = strtotime($wall->created_at);
+
+    return $wall;
   }
 }
