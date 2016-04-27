eea4e45f0d473aa4f29bc076970e177c9835cb95
De Pelsmaeker Jonas <jonas.de.pelsmaeker>
2016-04-27 12:17:15 +0200

    API: walls kunnen opgevraagd worden

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,14 +15,14 @@ class ApiController extends Controller
 {
   public function walls(){
 
-    $walls = Wall::all();
+    $walls = DB::table('walls')->select('id', 'name', 'user_id', 'created_at')->get();
 
     foreach($walls as $wall){
-      //remove deleted_at property
-      unset($wall->deleted_at);
 
-      //remove password
-      unset($wall->password);
+      //user_id formatting to user{user_id, name}
+      $wall->wall_id = $wall->id;
+      unset($wall->id);
+      $wall->creator = ["user_id" => $wall->user_id, "name" => "moet nog gebeuren"];
 
       //Convert timestamp to unix format
       $wall->created_at = strtotime($wall->created_at);
@@ -46,9 +46,9 @@ class ApiController extends Controller
     //format results to match JSON-document
     foreach($db as $usr){
 
+      //user_id formatting to user{user_id, name}
       $temp_userid = $usr->user_id;
       unset($usr->user_id);
-
       $usr->user = ["user_id" => $temp_userid, "name" => "moet nog gebeuren"];
 
       //Convert timestamp to unix format
