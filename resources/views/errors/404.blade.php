202f503c723c6e371c281ced8ad5ef3ec5403f21
De Pelsmaeker Jonas <jonas.de.pelsmaeker>
2016-04-29 10:37:30 +0200

    Done: custom 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,10 +1,21 @@
 @extends('masterlayout')
 
-@section('title', 'Blacklist')
+@section('title', '404')
+
+@section('page-script')
+<style>
+p, h1, marquee {
+    font-family: "Comic Sans MS";
+}
+</style>
+@stop
 
 @section('content')
 <div class="text-center">
   <h1>Oh no!</h1>
   <p>You lost The Great Wall to the Mongols.</p>
+  <img src="{{asset('img/team.jpg')}}" alt="Team Photo">
+  <p>Error 404: Page not found.</p>
+  <marquee>Oopsie.</marquee>
 </div>
 @stop
