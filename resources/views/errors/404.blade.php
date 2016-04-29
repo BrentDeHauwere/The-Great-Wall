2549b8d614139c7d03024cd08b81b7efbe5e1b76
De Pelsmaeker Jonas <jonas.de.pelsmaeker>
2016-04-29 12:32:25 +0200

    Favicon aangepast en 404 upgrade

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,6 +8,14 @@ p, h1, marquee {
     font-family: "Comic Sans MS";
 }
 </style>
+<script>
+  var random_images_array = ["siege.jpeg", "team.jpg"];
+
+  var num = Math.floor( Math.random() * imgAr.length );
+  var img = imgAr[ num ];
+  var imgStr = '<img src="' + img + '" alt = "error image">';
+  document.write(imgStr); document.close();
+</script>
 @stop
 
 @section('content')
@@ -16,6 +24,9 @@ p, h1, marquee {
   <p>You lost The Great Wall to the Mongols.</p>
   <img src="{{asset('img/team.jpg')}}" alt="Team Photo">
   <p>Error 404: Page not found.</p>
-  <marquee behavior="alternate">Oopsie.</marquee>
+  <marquee behavior="alternate">
+    <img src="{{asset('img/arco.png')}}" alt="Team Photo">
+    GEEN PANIEK IK BEN STUDENTENVERTEGENWOORDIGER
+  </marquee>
 </div>
 @stop
