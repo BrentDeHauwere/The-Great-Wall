a65ce7e81b6ee10a7fb3581dcd7d535b46a4b3c5
De Pelsmaeker Jonas <jonas.de.pelsmaeker>
2016-04-29 10:19:49 +0200

    WIP custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('masterlayout')
+
+@section('title', 'Blacklist')
+
+@section('content')
+<div class="text-center">
+  <h1>Oh no!</h1>
+  <p>You lost The Great Wall to the Mongols.</p>
+</div>
+@stop
