1a3dba6ea3d82ece5dc2ad552e0c8514d0ad73a7
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-05-10 14:49:08 +0200

    Added open_until to index view

--- resources/views/sessions/index.blade.php
+++ resources/views/sessions/index.blade.php
@@ -14,6 +14,7 @@
 				<td>User ID</td>
 				<td>Name</td>
 				<td>Protected</td>
+				<td>Open until</td>
 				<td>Actions</td>
 			</tr>
 		</thead>
@@ -24,9 +25,10 @@
 					<td>{{ $value->user_id }}</td>
 					<td>{{ $value->name }}</td>
 					<td>{{ $value->password }}</td>
+					<td>{{ $value->open_until }}</td>
 					<td>
 							<a class="btn btn-success btn-block" href="{{ action('SessionController@show', $value->id) }}">Show</a>
-							<a class="btn btn-info btn-block" href="{{ URL::to('/sessions/' . $value->id . '/edit') }}">Edit</a>
+							<a class="btn btn-info btn-block" href="{{ action('SessionController@edit', $value->id) }}">Edit</a>
 							<form action="{{action('SessionController@destroy', $value->id)}}" method="post">
 								<input type="hidden" name="_token" value="{{ csrf_token() }}"/>
 								<input type="hidden" name="_method" value="delete"/>
