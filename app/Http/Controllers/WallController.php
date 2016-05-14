7a115dd90ced8a083f614f0ed16e0fddf44fe344
jonas.de.pelsmaeker <jonas.de.pelsmaeker@student.ehb.be>
2016-05-14 10:24:41 +0200

    Fix: userid meegeven bij het aanmaken van een wall, wordt nu toegekent aan geauthenticeerde user.

--- app/Http/Controllers/WallController.php
+++ app/Http/Controllers/WallController.php
@@ -58,6 +58,9 @@ class WallController extends Controller
 	public function show($id)
 	{
 		$wall = Wall::findOrFail($id);
+
+		//dd($wall);
+
 		if ($wall->deleted_at != null || $wall->open_until == 0 || $wall->open_until < date('d-m-y H:i:s'))
 		{
 			abort(404);
