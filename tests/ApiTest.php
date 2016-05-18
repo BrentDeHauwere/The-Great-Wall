fb8fb0206e81dc0a023d3c01165c66a6caf85c02
Kamiel Klumpers <kamiel.klumpers@student.ehb.be>
2016-05-18 12:06:12 +0200

    Unit tests bijna klaar, commit zodat ik op PC kan werken

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -6,22 +6,65 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ApiTest extends TestCase
 {
-    /*
-     *  API routes
-     *  Route::get('walls', 'ApiController@walls');
+	/*
+	 *  API routes
+	 *  Route::get('walls', 'ApiController@walls');
+	 *
 		Route::get('messages', 'ApiController@messages');
 		Route::get('polls', 'ApiController@polls');
 		Route::get('blacklist', 'ApiController@blacklist');
 		Route::get('{requestedPage}', 'ApiController@badPage');
-     * */
+	 * */
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+	public function testWalls()
+	{
+		$this->get('/api/walls')
+			->seeJsonStructure([
+				'data' => [
+					'*' => [
+						'name',
+						'description',
+						'hashtag',
+						'created_at',
+						'wall_id',
+						'creator' => [
+							'user_id',
+							'name',
+						],
+					],
+				],
+			]);
+	}
+
+	public function testMessages()
+	{
+		$this->get('api/messages')
+			->seeJsonStructure([
+				'data' => [
+					'*' => [
+						'moderator_id',
+						'text',
+						'created_at',
+						'message_id',
+						'creator'  => [
+							'user_id',
+							'name',
+						],
+						'votes',
+						'question',
+						'wall'    => [
+							'name',
+							'description',
+							'hashtag',
+							'created_at',
+							'wall_id',
+							'creator' => [
+								'user_id',
+								'name',
+							]
+						]
+					]
+				]
+			]);
+	}
 }
