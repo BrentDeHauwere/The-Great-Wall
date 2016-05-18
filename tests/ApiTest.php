77dd5ba5bdf396dfd2ff73e8e4bcc187f17f212c
KamielK <sailorboy8425@live.be>
2016-05-18 22:08:02 +0200

    testCases volledig functioneel

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -6,65 +6,141 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ApiTest extends TestCase
 {
-	/*
-	 *  API routes
-	 *  Route::get('walls', 'ApiController@walls');
-	 *
-		Route::get('messages', 'ApiController@messages');
-		Route::get('polls', 'ApiController@polls');
-		Route::get('blacklist', 'ApiController@blacklist');
-		Route::get('{requestedPage}', 'ApiController@badPage');
-	 * */
+  /*
+   * Tests for each available route.
+   * */
 
-	public function testWalls()
-	{
-		$this->get('/api/walls')
-			->seeJsonStructure([
-				'data' => [
-					'*' => [
-						'name',
-						'description',
-						'hashtag',
-						'created_at',
-						'wall_id',
-						'creator' => [
-							'user_id',
-							'name',
-						],
-					],
-				],
-			]);
-	}
+    public function testWalls()
+    {
+        $response = $this->call('GET', '/api/walls');
+        $json = json_decode($response->content(), true);
 
-	public function testMessages()
-	{
-		$this->get('api/messages')
-			->seeJsonStructure([
-				'data' => [
-					'*' => [
-						'moderator_id',
-						'text',
-						'created_at',
-						'message_id',
-						'creator'  => [
-							'user_id',
-							'name',
-						],
-						'votes',
-						'question',
-						'wall'    => [
-							'name',
-							'description',
-							'hashtag',
-							'created_at',
-							'wall_id',
-							'creator' => [
-								'user_id',
-								'name',
-							]
-						]
-					]
-				]
-			]);
-	}
+        for ($i = 0; $i < $json['last_page']; $i++) {
+            $this->get('/api/walls?page=' . $i)
+                ->seeJsonStructure([
+                    'data' => [
+                        '*' => [
+                            'name',
+                            'description',
+                            'hashtag',
+                            'created_at',
+                            'wall_id',
+                            'creator' => [
+                                'user_id',
+                                'name',
+                            ],
+                        ],
+                    ],
+                ]);
+        }
+    }
+
+    public function testMessages()
+    {
+        $response = $this->call('GET', '/api/messages');
+        $json = json_decode($response->content(), true);
+
+        for ($i = 0; $i < $json['last_page']; $i++) {
+            $this->get('api/messages?page=' . $i)
+                ->seeJsonStructure([
+                    'data' => [
+                        '*' => [
+                            'moderator_id',
+                            'text',
+                            'created_at',
+                            'message_id',
+                            'creator',
+                            'votes',
+                            'question',
+                            'wall' => [
+                                'name',
+                                'description',
+                                'hashtag',
+                                'created_at',
+                                'wall_id',
+                                'creator' => [
+                                    'user_id',
+                                    'name',
+                                ]
+                            ]
+                        ]
+                    ]
+                ]);
+        }
+    }
+
+    public function testPolls()
+    {
+        $response = $this->call('GET', '/api/polls');
+        $json = json_decode($response->content(), true);
+
+        for ($i = 0; $i < $json['last_page']; $i++) {
+            echo $i;
+            $this->get('api/polls?page=' . $i)
+                ->seeJsonStructure([
+                    'data' => [
+                        '*' => [
+                            'moderator_id',
+                            'channel_id',
+                            'created_at',
+                            'poll_id',
+                            'wall' => [
+                                'name',
+                                'description',
+                                'hashtag',
+                                'created_at',
+                                'wall_id',
+                                'creator' => [
+                                    'user_id',
+                                    'name'
+                                ]
+                            ],
+                            'creator',
+                            'text',
+                            'options' => [
+                                '*' => [
+                                    'text',
+                                    'votes'
+                                ]
+                            ]
+                        ]
+                    ]
+                ]);
+        }
+    }
+
+    public function testBlacklist()
+    {
+        $response = $this->call('GET', '/api/blacklist');
+        $json = json_decode($response->content(), true);
+
+        for ($i = 0; $i < $json['last_page']; $i++) {
+            echo $i;
+            $this->get('api/blacklist?page=' . $i)
+                ->seeJsonStructure([
+                    'data' => [
+                        '*' => [
+                            'id',
+                            'reason',
+                            'created_at',
+                            'user' => [
+                                'user_id',
+                                'name'
+                            ]
+                        ]
+                    ]
+                ]);
+        }
+    }
+
+    public function testBadPage(){
+        $rndstr = str_random(20);
+
+        $this->get('/api/'.$rndstr)
+            ->seeJsonEquals([
+                'error' => 'URL not found.',
+                'error_code' => 404,
+                'reason' => 'The requested page '.$rndstr.' could not be found.'
+            ]);
+    }
 }
