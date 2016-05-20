f244faa6dc6d08f32a7e4c3e166802c3f361027e
De Pelsmaeker Jonas <jonas.de.pelsmaeker>
2016-05-20 16:46:22 +0200

    Jquery Tags (wip)

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -41,7 +41,7 @@
 			</div>
 
 			<div class="field">
-				<label for="hashtag">Hashtag (only if session is not password protected)</label>
+				<label for="hashtag">Twitter Hashtag (only if session is not password protected)</label>
 				<div class="ui labeled input">
 					<div class="ui basic blue label">
 						#
@@ -49,6 +49,29 @@
 					<input type="text" placeholder="TalkLaravel" name="hashtag" id="hashtag" value="{{ old('hashtag') }}">
 				</div>
 				@helper('hashtag')
+			</div>
+
+			<div class="field">
+				<label for="tags">Tags</label>
+				<div class="ui labels" id="tagrow">
+					<a class="ui label">
+						Talk
+						<i class="delete icon"></i>
+					</a>
+					<a class="ui label">
+						EHackB
+						<i class="delete icon"></i>
+					</a>
+				</div>
+				<div class="ui right labeled left icon input">
+					<i class="tags icon"></i>
+					<input type="text" placeholder="Your tag" id="tagInputField">
+					<a class="ui tag label" id="addTagButton">
+						Add Tag
+					</a>
+				</div>
+				<input type="hidden" value="Talk;EHackB;" name="tags" id="tagForm">
+				</input>
 			</div>
 
 			<div class="field">
@@ -80,4 +103,25 @@
 			<button class="ui button primary right floated" type="submit">Submit</button>
 		</form>
 	</div>
+@endsection
+
+@section('footer')
+	<script>
+		$("#addTagButton").click(function() {
+			var input = $("#tagInputField").val();
+			var output = '<a class="ui label">' +  input + '<i class="delete icon"></i></a>';
+			if (input != ""){
+				$( "#tagrow" ).prepend(output);
+				$( "#tagForm" ).val($("#tagForm").val() + input + ";");
+				$( "#tagInputField" ).val("");
+			}
+		});
+
+		$(".delete").click(function(){
+			$(this).parent().remove();
+			var value = $("#tagForm").val();
+			value.replace($(this).val(), '');
+			console.log($(this));
+		});
+	</script>
 @endsection
