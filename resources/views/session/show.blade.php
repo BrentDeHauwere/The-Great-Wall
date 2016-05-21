29bceeb87a613aea2da458327dc275c9ae6360c3
eli.boey <eli.boey@student.ehb.be>
2016-05-21 11:41:19 +0200

    Merge conflicts session/show

--- resources/views/session/show.blade.php
+++ resources/views/session/show.blade.php
@@ -5,7 +5,6 @@
 @section('page','moderate')
 
 @section('header')
-<<<<<<< HEAD
 <script src="https://10.3.50.20:1338/socket.io/socket.io.js"></script>
 
 @if(!empty($wall))
@@ -79,7 +78,6 @@
 		});
 		socket.on('msg1.polls.{{$wall->id}}:App\\Events\\NewPollEvent', function (data)
 		{
-<<<<<<< HEAD
 			var token = $('#token').val();
 			var e = '<div class="card">';
 			e += '<div class="content">';
@@ -129,132 +127,23 @@
 		var socket = io('http://10.3.50.20:1338');
 		socket.on('msg1.msg.{{$wall->id}}:App\\Events\\NewMessageEvent', function (data)
 		{
-			if (data.message.question_id == null)
-			{
-				var token = $('#token').val();
-				var e = '<div class="card">';
-				e += '<div class="content">';
-				e += '<img class="right floated mini ui image" src="http://semantic-ui.com/images/avatar/large/elliot.jpg">';
-				e += '<div class="header">'+ data.message.user.name +'</div>';
-				e += '<div class="meta">'+ data.message.wall.name+'</div>';
-				e += '<div class="description">'+data.message.text+'</div>';
-				e += '</div>';
-				e += '<div class="extra content">';
-				e += '<div class="ui three buttons">';
-				e += '<button type="submit" class="ui basic green button form="M_'+ data.message.id +'_A">Approve</button>';
-				e += '<button type="submit" class="ui basic red button" form="M_'+data.message.id+'_D">Decline</button>';
-				e += '<button class="ui basic grey button form="{{ "M_'data.message.id'_B" }}">Block</button>';
-				e += '</div>';
-				e += '<form method="post" action="" id="M_'+data.message.id+'_A">';
-				e += '<input type="hidden" name="_token" value="' + token + '">';
-				e += '<input type="hidden" name="message_id" value="'+data.message.id+'">';
-				e += '</form><form method="post" action="" id="{{ "M_'+data.message.id+'_D" }}">';
-				e += '<input type="hidden" name="_token" value="">';
-				e += '<input type="hidden" name="message_id" value="'+data.message.id+'">';
-				e += '</form><form method="get" action="" id="{{ "M_'+data.message.id+'_B" }}">';
-				e += '<input type="hidden" name="message_id" value="'+data.message.id+'">';
-				e += '</form>';
-				e += '</div>';
-				e += '</div>';
-				$(e).prependTo("#holder").hide().fadeIn(1500);
-			}
-			else if (data.message.question_id != null)
-			{
-				var token = $('#token').val();
-				var e = '<div class="card">';
-				e += '<div class="content">';
-				e += '<img class="right floated mini ui image" src="http://semantic-ui.com/images/avatar/large/elliot.jpg">';
-				e += '<div class="header">'+ data.message.user.name +'</div>';
-				e += '<div class="meta">'+ data.message.wall.name+'</div>';
-				e += '<div class="description">'+data.message.text;
-				e += '<div class="ui divider"></div>';
-				e += '<div class="meta">Answer on message</div>';
-				e += data.message.question.text;
-				e += '</div>';
-				e += '</div>';
-				e += '<div class="extra content">';
-				e += '<div class="ui three buttons">';
-				e += '<button type="submit" class="ui basic green button form="M_'+ data.message.id +'_A">Approve</button>';
-				e += '<button type="submit" class="ui basic red button" form="M_'+data.message.id+'_D">Decline</button>';
-				e += '<button class="ui basic grey button form="{{ "M_'data.message.id'_B" }}">Block</button>';
-				e += '</div>';
-				e += '<form method="post" action="" id="M_'+data.message.id+'_A">';
-				e += '<input type="hidden" name="_token" value="'+token+'">';
-				e += '<input type="hidden" name="message_id" value="'+data.message.id+'">';
-				e += '</form><form method="post" action="" id="{{ "M_'+data.message.id+'_D" }}">';
-				e += '<input type="hidden" name="_token" value="">';
-				e += '<input type="hidden" name="message_id" value="'+data.message.id+'">';
-				e += '</form><form method="get" action="" id="{{ "M_'+data.message.id+'_B" }}">';
-				e += '<input type="hidden" name="message_id" value="'+data.message.id+'">';
-				e += '</form>';
-				e += '</div>';
-				e += '</div>';
-				$(e).prependTo("#holder").hide().fadeIn(1500);
-			}
+			var e = '<div>'+ +'</div>';
+			$(e).prependTo("#holder").hide().fadeIn(1500);
 		});
 		socket.on('msg1.polls.{{$wall->id}}:App\\Events\\NewPollEvent', function (data)
 		{
 			var e = '<div>'+ +'</div>';
-=======
-			var e = '<div>' + +'</div>';
->>>>>>> master
-			$("#holder").append(e);
+			$(e).prependTo("#holder").hide().fadeIn(1500);
 		});
 		socket.on('msg1.choice.polls.{{$wall->id}}:App\\Events\\NewPollChoiceEvent', function (data)
 		{
-<<<<<<< HEAD
-			var e = '<div>'+ +'</div>';
-			$("#holder").append(e);
+			var e = '<div>' + +'</div>';
+			$(e).prependTo("#holder").hide().fadeIn(1500);
 		});
 	</script>
 	@endforeach
 	<?php $wall = null ?>
 @endif
-
-@stop
-
-@section('content')
-    <script>
-        $(document).ready(function () {
-            $(window).resize(function () {
-                $(".stackable").removeClass('four').removeClass('three').removeClass('two');
-                if ($(window).width() < 1000) {
-                    $(".stackable").addClass('two');
-                }
-                else if ($(window).width() < 1300) {
-                    $(".stackable").addClass('three');
-                }
-                else {
-                    $(".stackable").addClass('four');
-                }
-            });
-        });
-    </script>
-		<input type=hidden id="token" value="{{ csrf_token() }}"/>
-    <div class="body_customized">
-        @if(!empty($wall))
-            <h1>Session: {{ $wall->name }}</h1>
-        @else
-            <h1>
-                Sessions:
-                {{ $walls[0]->name }}
-                @for($i = 1;$i < $walls->count();$i++)
-                    {{ ", ".$walls[$i]->name}}
-                @endfor
-            </h1>
-        @endif
-
-
-        <div class="ui cards four stackable" id="holder">
-            @foreach($posts as $post)
-                @if($post[0]=='m')
-                    <div class="card">
-                        <div class="content">
-=======
-			var e = '<div>' + +'</div>';
-			$("#holder").append(e);
-		});
-	</script>
 @stop
 
 @section('content')
@@ -300,32 +189,7 @@
 				@if($post[0]=='m')
 					<div class="card">
 						<div class="content">
->>>>>>> master
-
 							<img class="right floated mini ui image" src="{{ route('user_images', ['filename' => $post[1]->user->id]) }}">
-
-<<<<<<< HEAD
-                            <div class="header">
-                                {{ $post[1]->user->name }}
-                            </div>
-                            <div class="meta">
-															@if(count($post[1]->question))
-																<i class="comments icon"></i>
-															@else
-																<i class="comment icon"></i>
-															@endif
-                              {{ $post[1]->wall->name }}
-                            </div>
-                            <div class="description">
-                                {{ $post[1]->text }}
-                                @if(count($post[1]->question))
-                                    <div class="ui divider"></div>
-                                    {{$post[1]->question->text}}
-                                @endif
-                            </div>
-                        </div>
-                        <div class="extra content">
-=======
 							<div class="header">
 								{{ \App\User::find($post[1]->user_id)->name }}
 							</div>
@@ -346,7 +210,6 @@
 							</div>
 						</div>
 						<div class="extra content">
->>>>>>> master
 
 							<div class="ui three buttons">
 								<button type="submit"
